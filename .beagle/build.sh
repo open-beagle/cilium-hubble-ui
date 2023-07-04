f3214866e47d89bfc54757fbf4a54cda2ed32cfe
shucheng <shucheng@bd-apaas.com>
2023-07-04 23:33:20 +0800

    v6.1.0 add backend

--- .beagle/build.sh
+++ .beagle/build.sh
@@ -9,25 +9,25 @@ cd $root/backend
 export CGO_ENABLED=0
 
 export GOARCH=amd64
-mkdir -p "$root/build/linux-$GOARCH/"
-go build -ldflags "-s -w" -o "$root/build/linux-$GOARCH/backend" github.com/cilium/hubble-ui/backend
+mkdir -p "$root/.tmp/linux-$GOARCH/"
+go build -ldflags "-s -w" -o "$root/.tmp/linux-$GOARCH/backend" github.com/cilium/hubble-ui/backend
 
 export GOARCH=arm64
 export CC=aarch64-linux-gnu-gcc
-mkdir -p "$root/build/linux-$GOARCH/"
-go build -ldflags "-s -w" -o "$root/build/linux-$GOARCH/backend" github.com/cilium/hubble-ui/backend
+mkdir -p "$root/.tmp/linux-$GOARCH/"
+go build -ldflags "-s -w" -o "$root/.tmp/linux-$GOARCH/backend" github.com/cilium/hubble-ui/backend
 
 export GOARCH=ppc64le
 export CC=powerpc64le-linux-gnu-gcc
-mkdir -p "$root/build/linux-$GOARCH/"
-go build -ldflags "-s -w" -o "$root/build/linux-$GOARCH/backend" github.com/cilium/hubble-ui/backend
+mkdir -p "$root/.tmp/linux-$GOARCH/"
+go build -ldflags "-s -w" -o "$root/.tmp/linux-$GOARCH/backend" github.com/cilium/hubble-ui/backend
 
 export GOARCH=mips64le
 export CC=mips64el-linux-gnuabi64-gcc
-mkdir -p "$root/build/linux-$GOARCH/"
-go build -ldflags "-s -w" -o "$root/build/linux-$GOARCH/backend" github.com/cilium/hubble-ui/backend
+mkdir -p "$root/.tmp/linux-$GOARCH/"
+go build -ldflags "-s -w" -o "$root/.tmp/linux-$GOARCH/backend" github.com/cilium/hubble-ui/backend
 
 export GOARCH=loong64
 export CC=loongarch64-linux-gnu-gcc
-mkdir -p "$root/build/linux-$GOARCH/"
-go build -ldflags "-s -w" -o "$root/build/linux-$GOARCH/backend" github.com/cilium/hubble-ui/backend
+mkdir -p "$root/.tmp/linux-$GOARCH/"
+go build -ldflags "-s -w" -o "$root/.tmp/linux-$GOARCH/backend" github.com/cilium/hubble-ui/backend
